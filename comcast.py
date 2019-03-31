db2755be1fae6db0519e78d8986fe599a086bfca
detroittommy879 <45469533+detroittommy879@users.noreply.github.com>
2019-03-31 12:25:46 -0400

    Add files via upload

---
+++ comcast.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import subprocess
+import time
+import random
+
+wifid = 'testtest'
+
+# wifid='wlxc83a35c2dd6b'
+
+mac = ':'.join(("%12x" % random.randint(0, 0xFFFFFFFFFFFF))[i:i+2] for i in range(0, 12, 2))
+
+print("New mac address will be " + mac)
+
+com1 = "ip link set dev " + wifid + " up"
+com2 = "ip link set dev " + wifid + " down"
+com3 = "macchanger -m " + mac + " " + "wifid"
+
+subprocess.run(com2, shell=True)
+time.sleep(3)
+subprocess.run(com3, shell=True)
+time.sleep(3)
+subprocess.run(com1, shell=True)
+
+# subprocess.run('ip', 'link', 'set', 'dev' , wifid, ' down')
+
+# print(com1)
+# print(com2)
+# print(com3)
+# macchanger -m 32:cf:cb:00:dd:f1 wlxc83a35c2dd6b
